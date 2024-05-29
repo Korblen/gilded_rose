2c87b45b5fc11a47245deaed25ffb5e5ee453ae8
Korblen <malo.bastianelli@gmail.com>
2024-05-29 23:45:21 +0200

    fini'

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,8 +1,7 @@
-const {Shop, Item} = require('../src/gilded_rose.js');
+const { Shop, Item } = require('../src/gilded_rose.js');
 
 describe("Gilded Rose", function() {
-
-  it("full test", () => {
+  it("should update the quality and sellIn correctly for all items", () => {
     const items = [
       new Item("+5 Dexterity Vest", 10, 20),
       new Item("Aged Brie", 2, 0),
@@ -12,13 +11,11 @@ describe("Gilded Rose", function() {
       new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
       new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
       new Item("Backstage passes to a TAFKAL80ETC concert", 5, 39),
-
-      // This Conjured item does not work properly yet
       new Item("Conjured Mana Cake", 3, 6),
     ];
 
-    const days = Number(process.argv[2]) || 2;;
     const gildedRose = new Shop(items);
+    const days = Number(process.argv[2]) || 2;
 
     for (let day = 0; day < days; day++) {
       console.log(`\n-------- day ${day} --------`);
@@ -28,9 +25,45 @@ describe("Gilded Rose", function() {
     }
   });
 
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+  it("should increase quality by 3 when there are 5 days or less for Backstage passes", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 45)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(48);
+  });
+
+  it("should not change the quality of Sulfuras", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 0, 80)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(80);
+  });
+
+  it("should degrade quality twice as fast after sellIn date has passed", function() {
+    const gildedRose = new Shop([new Item("Elixir of the Mongoose", 0, 10)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(8);
+  });
+
+  it("should not reduce quality below 0", function() {
+    const gildedRose = new Shop([new Item("Elixir of the Mongoose", 5, 0)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(0);
+  });
+
+  it("should increase the quality of Aged Brie over time", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 2, 0)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(1);
+  });
+
+  it("should not increase the quality of an item above 50", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 2, 50)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(50);
+  });
+
+  it("should degrade Conjured items twice as fast as normal items", function() {
+    const gildedRose = new Shop([new Item("Conjured Mana Cake", 3, 6)]);
+    gildedRose.updateQuality();
+    expect(gildedRose.items[0].quality).toBe(4);
   });
 });
